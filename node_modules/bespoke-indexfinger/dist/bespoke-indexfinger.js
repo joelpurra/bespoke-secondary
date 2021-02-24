9758fbb2dab9a0774bde37ecc826971ee13376df
Joel Purra <mig@joelpurra.se>
2021-02-24 16:03:04 +0100

    Update 2021-02-24T15:03:02.342Z

--- node_modules/bespoke-indexfinger/dist/bespoke-indexfinger.js
+++ node_modules/bespoke-indexfinger/dist/bespoke-indexfinger.js
@@ -1,5 +1,5 @@
 /*!
- * bespoke-indexfinger v1.0.0
+ * bespoke-indexfinger v1.0.1
  *
  * Copyright 2014, Joel Purra
  * This content is released under the MIT license
--- node_modules/bespoke-keys/dist/bespoke-keys.js
+++ node_modules/bespoke-keys/dist/bespoke-keys.js
@@ -1,7 +1,7 @@
 /*!
  * bespoke-keys v1.0.0
  *
- * Copyright 2014, Mark Dalgleish
+ * Copyright 2015, Mark Dalgleish
  * This content is released under the MIT license
  * http://mit-license.org/markdalgleish
  */
@@ -13,12 +13,13 @@ module.exports = function(options) {
 
     document.addEventListener('keydown', function(e) {
       if (e.which == 34 || // PAGE DOWN
-        e.which == 32 || // SPACE
+        (e.which == 32 && !e.shiftKey) || // SPACE WITHOUT SHIFT
         (isHorizontal && e.which == 39) || // RIGHT
         (!isHorizontal && e.which == 40) // DOWN
       ) { deck.next(); }
 
       if (e.which == 33 || // PAGE UP
+        (e.which == 32 && e.shiftKey) || // SPACE + SHIFT
         (isHorizontal && e.which == 37) || // LEFT
         (!isHorizontal && e.which == 38) // UP
       ) { deck.prev(); }
